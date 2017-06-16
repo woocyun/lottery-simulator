25b60283afa9dc9fdbaa5e5d6b2a84634953dccf
Woo Yun <woocyun@gmail.com>
2017-06-15 23:05:32 -0700

    init lottery list component

---
+++ src/app/simulator/components/lottery-list/lottery-list.component.js
@@ -0,0 +1,15 @@
+import template from './lottery-list.html';
+
+const LotteryListComponent = {
+  bindings: {
+
+  },
+  templateUrl: template,
+  controller: class LotteryListController {
+    constructor() {
+
+    }
+  }
+};
+
+export default LotteryListComponent;
--- src/app/simulator/simulator.module.js
+++ src/app/simulator/simulator.module.js
@@ -2,6 +2,7 @@ import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 
 import SimulatorPageComponent from './containers/simulator-page/simulator-page.component';
+import LotteryListComponent from './components/lottery-list/lottery-list.component';
 
 const SimulatorModule = angular
   .module('components.simulator', [
@@ -14,6 +15,7 @@ const SimulatorModule = angular
         component: 'simulatorPage'
       });
   })
-  .component('simulatorPage', SimulatorPageComponent);
+  .component('simulatorPage', SimulatorPageComponent)
+  .component('lotteryList', LotteryListComponent);
 
 export default SimulatorModule;
