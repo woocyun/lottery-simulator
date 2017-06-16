96fd973c96cfa3724c60a85296ac6461c2f0d3ca
Woo Yun <woocyun@gmail.com>
2017-06-16 00:08:06 -0700

    register ngRedux and create store

--- src/app/root.module.js
+++ src/app/root.module.js
@@ -1,12 +1,20 @@
 import '../sass/styles.scss';
 import angular from 'angular';
+import ngRedux from 'ng-redux';
+import rootReducer from './root.reducer';
 import RootComponent from './root.component';
 import SimulatorModule from './simulator/simulator.module';
 
 const RootModule = angular
   .module('root', [
+    ngRedux,
     SimulatorModule.name
   ])
   .component('root', RootComponent);
+
+  RootModule
+    .config(/*@ngInject*/ ($ngReduxProvider) => {
+      $ngReduxProvider.createStoreWith(rootReducer, []);
+    });
 
 export default RootModule;
