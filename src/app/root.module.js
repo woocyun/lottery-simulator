95b423c66f886ec4cb54d2676d211be86efb8809
Woo Yun <woocyun@gmail.com>
2017-06-16 00:10:42 -0700

    import redux dev tools

--- src/app/root.module.js
+++ src/app/root.module.js
@@ -4,6 +4,8 @@ import ngRedux from 'ng-redux';
 import rootReducer from './root.reducer';
 import RootComponent from './root.component';
 import SimulatorModule from './simulator/simulator.module';
+import createLogger from 'redux-logger';
+import { default as DevTools, runDevTools } from './devTools';
 
 const RootModule = angular
   .module('root', [
@@ -12,9 +14,18 @@ const RootModule = angular
   ])
   .component('root', RootComponent);
 
+if (process.env.NODE_ENV === 'development') {
+  RootModule
+    .config(/*@ngInject*/ ($ngReduxProvider) => {
+      $ngReduxProvider.createStoreWith(rootReducer, []);
+      $ngReduxProvider.createStoreWith(rootReducer, [ createLogger() ], [ DevTools.instrument() ]);
+    })
+    .run(runDevTools);
+} else {
   RootModule
     .config(/*@ngInject*/ ($ngReduxProvider) => {
       $ngReduxProvider.createStoreWith(rootReducer, []);
     });
+}
 
 export default RootModule;
